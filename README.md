dd88967b60dbe0e28ebb8cd68749e78bd29d247a
Jacob Parris <typefortyproductions@gmail.com>
2021-10-31 17:13:56 -0400

    Updated Readme with some planning

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Fresh_Splat_Box
+
+
+# TODO List:
+
+1. Finish up CRUD for Formal Scrolls
+2. Once Complete, begin next iterations:
+
+# Formal Scrolls
+   * Finish CRUD first go
+   * Add sections to Formal Scroll Model:
+        * Success, Failure, Alt Base Spread
+        * Casting Time
+        * Duration
+        * Type
+        * Vessel
+        * Split Components (Primary, Secondary)
+        * In Play Effect Description
+        * Stat Blocks for Guardians/Created monsters
+        * Alteration List
+        * Rarity
+# Formal Components
+    * create formal component model with Following Sections:
+        * id (auto)
+        * component_name
+        * school
+        * base_cost
+        * base_coin
+        * rariy
+        * scroll
+        * notes
+        * Flavor Text?
+# Production Items
+*   *   *   
+    *   Create Production Model with following sections:
+        *   id (auto)
+        *   item_name
+        *   type (Alchemy, Poison)
+        *   point_cost
+        *   cost_multiplier
+        *   base_coin
+        *   rarity
+        *   effect duration
+        *   effect description
+# Spells
--- models/Formal_Scroll.js
+++ models/Formal_Scroll.js
@@ -1,55 +1,57 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Formal_Scroll extends Model {}
 
-    Formal_Scroll.init(
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primaryKey: true,
-                autoIncrement: true,
-            },
-            scroll_name: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            school: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            level: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            base_cost: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            base_coin: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            shot_status: {
-                type: DataTypes.BOOLEAN,
-            },
-            component: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            notes: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            }
-        },
-        {
-            sequelize,
-            timestamps: false,
-            freezeTableName: true,
-            underscored: true,
-            modelName: 'formal_scroll',
-        }
-    );
-    
-    module.exports = Formal_Scroll;
+Formal_Scroll.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    scroll_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    school: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    level: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    base_cost: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    base_coin: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    shot_status: {
+      type: DataTypes.BOOLEAN,
+    },
+    component: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    notes: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    //TODO: Add additional columns to this model for Formal Scrolls: 
+    //Base SFA spread, Casting Time, Duration, Type, Vessel, Primary/Secondary Component Split, In Play Description, Alteration List
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "formal_scroll",
+  }
+);
+
+module.exports = Formal_Scroll;
